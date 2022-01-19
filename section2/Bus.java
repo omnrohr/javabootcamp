2c299bec6ebe022295ed7cea5f557f43f71d50c0
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-01-19 14:31:27 +0200

    section 2

---
+++ section2/Bus.java
@@ -0,0 +1,13 @@
+public class Bus {
+    public static void main(String[] args) {
+        int passengers = 0;
+        String sentence = "Total passengers are:";
+        passengers += 9;
+        System.out.println(sentence + passengers);
+        passengers -= 5;
+        System.out.println(sentence + passengers);
+        passengers -= 4;
+        System.out.println(sentence + passengers);
+    }
+
+}
---
+++ section2/JoiningStrings.java
@@ -0,0 +1,10 @@
+public class JoiningStrings {
+    public static void main(String[] args) {
+        String sentence = "His name is: ";
+        String placeholder = "Harry";
+        System.out.println(sentence + placeholder);
+
+        int points = 50;
+        System.out.println("Ponts of Gryffin is: " + points);
+    }
+}
