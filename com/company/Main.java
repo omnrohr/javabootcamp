d43d47ccd5517b02154afe8141498af2705e2ba0
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-01-21 19:42:12 +0200

    section 4

---
+++ com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(" SSS " + "    " + "  A  ");
+        System.out.println("S   S" + "    " + " A A ");
+        System.out.println("S    " + "    " + "A   A");
+        System.out.println(" SSS " + "    " + "AAAAA");
+        System.out.println("    S" + "    " + "A   A");
+        System.out.println("S   S" + "    " + "A   A");
+        System.out.println(" SSS " + "    " + "A   A");
+    }
+}
---
+++ section4/Chorus.java
@@ -0,0 +1,17 @@
+package section4;
+
+public class Chorus {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            chorus();
+        }
+    }
+
+    public static void chorus() {
+        System.out.println("Don't blame it on the sunshine");
+        System.out.println("Don't blame it on the moonlight");
+        System.out.println("Don't blame it on good times'");
+        System.out.println("blame it on the boogie\n");
+    }
+
+}
