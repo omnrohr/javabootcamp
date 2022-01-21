666350c29bc52f4a27ac1f36476c3801f909f697
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-01-21 18:36:01 +0200

    DealerShip

--- section3/DealerShip.java
+++ section3/DealerShip.java
@@ -12,11 +12,40 @@ public class DealerShip {
         String option = scanner.nextLine();
 
         switch (option) {
-            case "a":
-                System.out.println("\nYour choice is option " + option);
+            case "a": {
+                System.out.println("\nWhat is your budjet?");
+                int budget = scanner.nextInt();
+                scanner.nextLine();
+                if (budget >= 10000) {
+                    System.out.println("\n\tGreat A Nisan altima is available");
+                    System.out.println("\nDo you have insurance?, type 'yes' or 'no'.");
+                    String insurance = scanner.nextLine();
+                    System.out.println("\nDo you have a Licenses?, type 'yes' or 'no'.");
+                    String licenses = scanner.nextLine();
+                    System.out.println("\nWhat is your credit score?, type 'yes' or 'no'.");
+                    int creditScore = scanner.nextInt();
+                    scanner.nextLine();
+                    if (insurance.equals("yes") && licenses.equals("yes") && creditScore > 660) {
+                        System.out.println("\nSold Pleasure doing business with you");
+                    } else {
+                        System.out.println("\nSorry, you are not eligible");
+                    }
+
+                } else {
+                    System.out.println("\tSorry we don't sell cars under $10,000");
+                }
                 break;
+            }
             case "b":
-                System.out.println("\nYour choice is option " + option);
+                System.out.println("\nWhat is your car value at? ");
+                int value = scanner.nextInt();
+                System.out.println("\nWhat is your selling price?");
+                int selling = scanner.nextInt();
+                if (value > selling && selling < 30000) {
+                    System.out.println("\nWe will buy your car. Pleasure to do business with you!");
+                } else {
+                    System.out.println("\nSorry we're not interested.");
+                }
                 break;
             default:
                 System.out.println("\nInvalid choice.");
