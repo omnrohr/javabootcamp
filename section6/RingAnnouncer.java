159e3c1dbe66ba203eb90cfeeee82b016507d9dd
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-01-23 11:21:07 +0200

    ring annoncer

---
+++ section6/RingAnnouncer.java
@@ -0,0 +1,35 @@
+public class RingAnnouncer {
+    public static void main(String[] args) {
+        String[] record = { "WIN", "WIN", "WIN", "LOSS", "WIN", "WIN", "LOSS" };
+        int wins = 0;
+        int losses = 0;
+        for (int i = 0; i < record.length; i++) {
+            if (record[i].equals("WIN")) {
+                wins++;
+            } else {
+                losses++;
+            }
+        }
+        /**
+         * Task 1
+         *
+         * Using a for loop:
+         * count Java's wins and store the result in a variable: wins.
+         * count Java's losses and store the result in a variable: losses.
+         *
+         *
+         *
+         */
+        System.out.println("\nWith a professional record of " + wins + " Wins and " + losses + " losses");
+        System.out.println("He is the pride of oracle: Java Fury!");
+        /**
+         * Task 2
+         * Print:
+         * • \nWith a professional record of <wins> Wins and <losses> losses.
+         * • He is the pride of oracle: Java Fury!
+         *
+         *
+         */
+
+    }
+}
