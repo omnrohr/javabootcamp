5b355b0f696c86f5c9bd1722ed401b2b4f027d17
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-01-23 11:21:46 +0200

    Tax

---
+++ section6/Tax.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Tax {
+    public static void main(String[] args) {
+        double[] price = { 1.99, 2.99, 3.99, 4.99 };
+        /**
+         * Task 1:
+         *
+         * Create a new array afterTax with the same length as price
+         *
+         */
+        double[] priceAfterTax = new double[price.length];
+        for (int i = 0; i < priceAfterTax.length; i++) {
+            priceAfterTax[i] = price[i] * 1.13;
+        }
+        /**
+         * Task 2:
+         *
+         * Update each element in afterTax to be: price + 13% * price.
+         *
+         */
+
+        /**
+         * Task 3:
+         *
+         * Print the following messages:
+         * The original prices are: <original prices>
+         * The prices after tax are: <after tax prices>
+         *
+         */
+        System.out.println("The original prices are: " + Arrays.toString(price));
+        System.out.println("The prices after tax are: " + Arrays.toString(priceAfterTax));
+
+    }
+}
