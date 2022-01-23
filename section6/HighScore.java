a1daa70bf75495d0065d4bcfec17826cce4cf96a
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-01-23 11:20:18 +0200

    High Score

---
+++ section6/HighScore.java
@@ -0,0 +1,56 @@
+public class HighScore {
+    public static void main(String[] args) {
+        int[] randoms = new int[10];
+        for (int i = 0; i < 10; i++) {
+            randoms[i] = randomNumber();
+            System.out.print(randoms[i] + " ");
+        }
+        System.out.println();
+        System.out.print("Here are the scores: ");
+        for (int i = 0; i < randoms.length; i++) {
+            System.out.print(randoms[i] + " ");
+        }
+        System.out.println();
+
+        /**
+         * Task 2
+         *
+         * 1. Create an array that stores 10 random numbers
+         * 2. Using a for loop, print the array elements on the same line.
+         * Example – Here are the scores: 14775 48328 7928 27102 21787 21063 38096 42711
+         * 32863 23707
+         */
+        int highScore = 0;
+
+        for (int i = 0; i < randoms.length; i++) {
+            if (randoms[i] > highScore) {
+                highScore = randoms[i];
+            }
+        }
+        System.out.println("\n\nThe highest score is: " + highScore + ". Give that man a cookie!\n");
+
+        /**
+         * Task 3
+         *
+         * 1. Using a for loop, find the highest score in the array and update the
+         * highScore.
+         * 2. println("\n\nThe highest score is: <>. Give that man a cookie!");
+         * 
+         */
+
+    }
+
+    /**
+     * Task 1
+     * Function name – randomNumber
+     * 
+     * @return random number (int)
+     *
+     *         Inside the function
+     *         - returns a random number between 0 and 50000
+     */
+    public static int randomNumber() {
+        return ((int) (Math.random() * 50000));
+    }
+
+}
