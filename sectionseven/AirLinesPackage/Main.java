cf09e57094f11ac70146b713653758265281402d
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-01-25 21:03:58 +0200

    fix setters and getters whit array trap a copy of the array, and addig the cheat sheet

--- sectionseven/AirLinesPackage/Main.java
+++ sectionseven/AirLinesPackage/Main.java
@@ -2,18 +2,37 @@ package AirLinesPackage;
 
 import java.util.Arrays;
 
+//
+//import java.util.Arrays;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        String[] passport = {"Rayan Slim", "Canadian", "01/01/1111"};
+//        Person rayan = new Person("Rayan Slim", "Canadian", "01/01/1111", passport,11 );
+//        System.out.println("this person: "+ rayan.getName()+" is: "+ rayan.getNationality()+" Born at: "+
+//                rayan.getDateOfBirth()+" holding passport: "+ Arrays.toString(rayan.getPassport()) + " reserved seat: "+ rayan.getSeatNumber());
+//        Person anonymous = new Person();
+//        anonymous.setName("Null");
+//        anonymous.setNationality("Null");
+//        anonymous.setDateOfBirth("Null");
+//        anonymous.setPassport(passport);
+//        anonymous.setSeatNumber(0);
+//
+//        Person person2 = new Person(rayan);
+//        System.out.println(person2);
+//    }
+//}
 public class Main {
     public static void main(String[] args) {
-        String[] passport = {"Rayan Slim", "Canadian", "01/01/1111"};
-        Person rayan = new Person("Rayan Slim", "Canadian", "01/01/1111", passport,11 );
-        System.out.println("this person: "+ rayan.getName()+" is: "+ rayan.getNationality()+" Born at: "+
-                rayan.getDateOfBirth()+" holding passport: "+ Arrays.toString(rayan.getPassport()) + " reserved seat: "+ rayan.getSeatNumber());
-        Person anonymous = new Person();
-        anonymous.setName("Null");
-        anonymous.setNationality("Null");
-        anonymous.setDateOfBirth("Null");
-        anonymous.setPassport(passport);
-        anonymous.setSeatNumber(0);
+        String[] passport = { "Rayan Slim", "Canadian", "01/01/1111" };
+        Person person = new Person("Rayan Slim", "Canadian", "01/01/1111", 1);
+        if (person.applyPassport() == true) {
+            System.out.println("Name: " + person.getName() + "\n" +
+                    "Nationality: " + person.getNationality() + "\n" +
+                    "Date of Birth: " + person.getDateOfBirth() + "\n" +
+                    "Seat Number: " + person.getSeatNumber() + "\n" + "Passport: " +
+                    Arrays.toString(person.getPassport()) + "\n");
+        }
 
     }
 }
--- sectionseven/AirLinesPackage/Person.java
+++ sectionseven/AirLinesPackage/Person.java
@@ -1,5 +1,7 @@
 package AirLinesPackage;
 
+import java.util.Arrays;
+
 public class Person {
     private String name;
     private String nationality;
@@ -7,21 +9,31 @@ public class Person {
     private String[] passport;
     private int seatNumber;
 
-    public Person(String name, String nationality, String dateOfBirth, String[] passport, int seatNumber) {
+    public Person(String name, String nationality, String dateOfBirth, int seatNumber) {
         this.name = name;
         this.nationality = nationality;
         this.dateOfBirth = dateOfBirth;
-        this.passport = passport;
+        this.passport = new String[3];
         this.seatNumber = seatNumber;
     }
-    public Person(){
-        this(null,null,null,null,0);
+
+    public Person() {
+        this(null, null, null, 0);
     }
 
-    boolean applyPassport(String passport){
+    public Person(Person source) {
+        this.name = source.name;
+        this.nationality = source.nationality;
+        this.dateOfBirth = source.dateOfBirth;
+        this.passport = Arrays.copyOf(source.passport, source.passport.length);
+        this.seatNumber = source.seatNumber;
+    }
+
+    boolean applyPassport() {
         return true;
     }
-    boolean chooseSeat(int seatNumber){
+
+    boolean chooseSeat(int seatNumber) {
         return true;
     }
 
@@ -50,11 +62,11 @@ public class Person {
     }
 
     public String[] getPassport() {
-        return passport;
+        return Arrays.copyOf(passport, passport.length);
     }
 
     public void setPassport(String[] passport) {
-        this.passport = passport;
+        this.passport = Arrays.copyOf(this.passport, this.passport.length);
     }
 
     public int getSeatNumber() {
@@ -63,5 +75,16 @@ public class Person {
 
     public void setSeatNumber(int seatNumber) {
         this.seatNumber = seatNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", passport=" + Arrays.toString(passport) +
+                ", seatNumber=" + seatNumber +
+                '}';
     }
 }
