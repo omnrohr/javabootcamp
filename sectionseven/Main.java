d205b7d1c55b8c40e9961d1cd5b12153ab3c7cc0
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-01-25 20:23:21 +0200

    adding Arrays to getters and setters and constructor

--- sectionseven/Main.java
+++ sectionseven/Main.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Car nissan = new Car();
-        Car dodge = new Car("Dodge", 11000, 2019, "Red");
+        String[] parts = { "wheel", "keys" };
+        Car nissan = new Car("Nissan", 8000, 2019, "Black", parts);
+        Car dodge = new Car("Dodge", 11000, 2019, "Red", parts);
         Car nissan2 = new Car(nissan);
         nissan2.setPrice(8000.00);
         nissan.setColor("Nissan");
@@ -9,10 +12,15 @@ public class Main {
         nissan.setYear(2010);
         nissan.setColor("blue");
         System.out.println("This " + nissan.getMake() + " is worth: $" + nissan.getPrice() + " it was built in: " +
-                nissan.getYear() + " It is: " + nissan.getColor() + " color.");
+                nissan.getYear() + " It is: " + nissan.getColor() + " color with parts " + nissan.getParts());
         System.out.println("This " + dodge.getMake() + " is worth: $" + dodge.getPrice() + " it was built in: " +
-                dodge.getYear() + " It is: " + dodge.getColor() + " color.");
+                dodge.getYear() + " It is: " + dodge.getColor() + " color with parts " + dodge.getParts());
         System.out.println(dodge);
         System.out.println(nissan2);
+        nissan.drive();
+        dodge.drive();
+        nissan2.drive();
+        nissan2.setParts(new String[] { "filter", "engine" });
+        System.out.println(Arrays.toString(nissan2.getParts()));
     }
 }
